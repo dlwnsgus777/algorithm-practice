25172f7bdad9664ecc32c547046f9f3180d00b08
LeeJuneHyun <dlwnsgus777@naver.com>
2020-06-06 09:36:07 +0900

    feat: 백준 알고리즘 문제 풀이 중

---
+++ baekjoon/src/main/java/basic/test1/AvgScrore.java
@@ -0,0 +1,23 @@
+package basic.test1;
+
+import java.util.Scanner;
+
+public class AvgScrore {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        int avg = 0;
+
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            if (a < 40) {
+                a = 40;
+            }
+            sum = sum + a;
+            count++;
+        }
+        avg = sum / count;
+        System.out.println(avg);
+    }
+}
