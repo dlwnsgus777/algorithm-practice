c920b23cfc231be38b1a014425bc307985efd980
Lee, JunHyun <dlwnsgus777@naver.com>
2020-06-08 10:57:01 +0900

    feat: 백준 알고리즘 문제 풀이 중

--- baekjoon/src/main/java/basic/test1/SecondNumber.java
+++ baekjoon/src/main/java/basic/test1/SecondNumber.java
@@ -6,6 +6,15 @@ public class SecondNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] arr = new int[3];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr)
+        }
         
     }
 }
