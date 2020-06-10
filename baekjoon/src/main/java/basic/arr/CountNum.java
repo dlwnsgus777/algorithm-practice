497f1edbab622b2babb6c6a5c9c50f9ef1bd1243
Lee, JunHyun <dlwnsgus777@naver.com>
2020-06-10 15:52:55 +0900

    feat: 백준 알고리즘 문제 풀이

---
+++ baekjoon/src/main/java/basic/arr/CountNum.java
@@ -0,0 +1,9 @@
+package basic.arr;
+
+import java.util.Scanner;
+
+public class CountNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ baekjoon/src/main/java/basic/arr/MaxNumIndex.java
@@ -0,0 +1,29 @@
+package basic.arr;
+
+import java.io.*;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class MaxNumIndex {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[9];
+        int max = -10000;
+        int index = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (max < num) {
+                max = num;
+                index = i + 1;
+            }
+        }
+
+        bw.write(max + "\n" + index);
+
+        bw.flush();
+    }
+}
