0b48de424c73efdf571b0426464635e06cdde112
Lee, JunHyun <dlwnsgus777@naver.com>
2020-06-30 16:52:41 +0900

    feat: 백준 알고리즘 문제 풀이 중

---
+++ baekjoon/src/main/java/basic/strings/Dial.java
@@ -0,0 +1,14 @@
+package basic.strings;
+
+import java.util.Scanner;
+
+public class Dial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+    }
+
+    public int dia {
+
+    }
+}
