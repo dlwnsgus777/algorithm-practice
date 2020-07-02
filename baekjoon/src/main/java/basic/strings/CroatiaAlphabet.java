f45bf5c1ac6742f4d06de0144194f177ccf891a3
Lee, JunHyun <dlwnsgus777@naver.com>
2020-07-02 09:21:40 +0900

    feat: 백준 알고리즘 문제 풀이

--- baekjoon/src/main/java/basic/strings/CroatiaAlphabet.java
+++ baekjoon/src/main/java/basic/strings/CroatiaAlphabet.java
@@ -1,7 +1,32 @@
 package basic.strings;
 
-public class CroatiaAlphabet {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class CroatiaAlphabet {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] word = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        String input = br.readLine().trim();
+
+        int result = 0;
+
+        for(int i = 0; i < word.length; i++) {
+            if(input.contains(word[i])) {
+                input = input.replaceAll(word[i], "*");
+                // System.out.println(input);
+            }
+        }
+        result = input.length();
+        bw.write(result + "");
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
