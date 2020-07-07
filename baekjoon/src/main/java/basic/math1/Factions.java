92b5ccb38f68bce288e86fe06f8acbb90bb7ca65
Lee, JunHyun <dlwnsgus777@naver.com>
2020-07-08 08:29:39 +0900

    feat: 백준 알고리즘 문제 풀이

---
+++ baekjoon/src/main/java/basic/math1/Factions.java
@@ -0,0 +1,32 @@
+package basic.math1;
+
+import java.util.Scanner;
+
+public class Factions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        int up = 0;
+        int down = 0;
+        String result = "";
+
+        while (i + sum < n) {
+            sum += i;
+            i++;
+        }
+
+        if (i % 2 == 0) {
+           up = n - sum;
+        } else {
+            up = i + 1 - (n - sum);
+        }
+
+        down = i + 1 - up;
+
+        result = up + "/" + down;
+        System.out.println(result);
+    }
+}
