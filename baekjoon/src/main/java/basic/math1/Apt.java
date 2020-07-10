13d018883d30e432b6b71596d05e49a2ec234df9
LeeJuneHyun <dlwnsgus777@naver.com>
2020-07-11 08:34:11 +0900

    feat: 백준 알고리즘 풀이 중

---
+++ baekjoon/src/main/java/basic/math1/Apt.java
@@ -0,0 +1,16 @@
+package basic.math1;
+
+import java.util.Scanner;
+
+public class Apt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int k = sc.nextInt();
+            int n = sc.nextInt();
+
+        }
+    }
+}
