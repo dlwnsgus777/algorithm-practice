8230b32b8da4f1420a9ab516133fee0d606c3580
LeeJuneHyun <dlwnsgus777@naver.com>
2020-07-12 06:40:03 +0900

    feat: 백준 알고리즘 문제 풀이

--- baekjoon/src/main/java/basic/math1/Apt.java
+++ baekjoon/src/main/java/basic/math1/Apt.java
@@ -7,10 +7,23 @@ public class Apt {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
+        int[][] apt = new int[15][15];
+
+        for (int i = 1; i < apt[0].length; i++) {
+            apt[0][i] = i;
+        }
+
+        for (int i = 1; i < apt.length; i++) {
+            for (int j = 1; j < apt[i].length; j++) {
+                apt[i][j] = apt[i - 1][j] + apt[i][j - 1];
+            }
+        }
+
         for (int i = 0; i < t; i++) {
             int k = sc.nextInt();
             int n = sc.nextInt();
 
+            System.out.println(apt[k][n]);
         }
     }
 }
