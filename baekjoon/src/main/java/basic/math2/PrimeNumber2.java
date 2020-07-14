693c373747d476342ca34f51d4c5d1db6344c23f
Lee, JunHyun <dlwnsgus777@naver.com>
2020-07-15 08:28:54 +0900

    feat: 백준 알고리즘 문제 풀이

---
+++ baekjoon/src/main/java/basic/math2/PrimeNumber2.java
@@ -0,0 +1,43 @@
+package basic.math2;
+
+import java.util.Scanner;
+
+public class PrimeNumber2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        int sum = 0;
+        int fir = 0;
+        boolean has = false;
+
+        for (int i = M; i <= N; i++) {
+            if (i == 1) {
+                continue;
+            }
+            boolean check = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                has = true;
+                if (sum == 0) {
+                    fir = i;
+                }
+                sum += i;
+            }
+        }
+
+        if (has) {
+            System.out.println(sum);
+            System.out.println(fir);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
