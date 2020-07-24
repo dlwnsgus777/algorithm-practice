d8a01645d40b10fd640adec7236839f4a7f81510
LeeJuneHyun <dlwnsgus777@naver.com>
2020-07-25 08:02:24 +0900

    feat: 백준 알고리즘 문제 풀이 중

---
+++ baekjoon/src/main/java/basic/bruteForce/BlackJack.java
@@ -0,0 +1,9 @@
+package basic.bruteForce;
+
+import java.util.Scanner;
+
+public class BlackJack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
