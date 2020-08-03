e4b03df76936b75b091bbfeb90c8c88fce064227
Lee, JunHyun <dlwnsgus777@naver.com>
2020-08-04 08:21:15 +0900

    feat: 백준 알고리즘 문제 풀이 중

---
+++ baekjoon/src/main/java/basic/sort/Statistics.java
@@ -0,0 +1,40 @@
+package basic.sort;
+
+import java.util.*;
+
+public class Statistics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        OptionalDouble avg = Arrays.stream(arr).average();
+
+        Set<Integer> all = new HashSet<>();
+        int[] duple = Arrays.stream(arr).filter(b -> !all.add(b)).toArray();
+
+        int third = 0;
+
+        if (duple.length <= 0) {
+            if (n == 1) {
+                third = arr[0];
+            } else {
+                third = arr[1];
+            }
+        } else {
+            third = duple[1];
+        }
+
+        System.out.println(Math.round(avg.getAsDouble()));
+        System.out.println(arr[n / 2]);
+        System.out.println(third);
+        System.out.println(Math.abs(arr[0] - arr[n - 1]));
+    }
+}
