464db374991704b80c278ca297fef7a67937a1b6
Lee, JunHyun <dlwnsgus777@naver.com>
2020-08-07 08:11:10 +0900

    feat: 백준 알고리즘 문제 풀이

---
+++ baekjoon/src/main/java/basic/sort/WordSort.java
@@ -0,0 +1,27 @@
+package basic.sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        Arrays.sort(arr, (a1, a2) -> {
+            if (a1.length() == a2.length()) {
+                return a1.compareTo(a2);
+            } else {
+                return a1.length() - a2.length();
+            }
+        });
+
+        Arrays.stream(arr).distinct().forEach(s -> System.out.println(s));
+    }
+}
