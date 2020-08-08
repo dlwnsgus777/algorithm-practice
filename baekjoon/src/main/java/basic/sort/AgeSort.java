014582752ab5d1e1c483eb3dd6bdf1ff5013c74d
LeeJuneHyun <dlwnsgus777@naver.com>
2020-08-09 08:49:20 +0900

    feat: 백준 알고리즘 문제 풀이 중

--- baekjoon/src/main/java/basic/sort/AgeSort.java
+++ baekjoon/src/main/java/basic/sort/AgeSort.java
@@ -1,7 +1,39 @@
 package basic.sort;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AgeSort {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String[][] arr = new String[n][2];
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = sc.next();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < 2; j++) {
+                System.out.print(arr[i][j]);
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < 2; j++) {
+                System.out.print(arr[i][j]);
+            }
+
+            System.out.println();
+        }
     }
 }
