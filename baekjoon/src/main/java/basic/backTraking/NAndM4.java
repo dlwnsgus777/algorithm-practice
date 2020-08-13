2200e815acd4d9ac251e51b7a60c8e6cfd6365e2
Lee, JunHyun <dlwnsgus777@naver.com>
2020-08-14 08:13:10 +0900

    feat: 백준 알고리즘 문제 풀이

---
+++ baekjoon/src/main/java/basic/backTraking/NAndM4.java
@@ -0,0 +1,32 @@
+package basic.backTraking;
+
+import java.util.Scanner;
+
+public class NAndM4 {
+    static int n, m;
+    static int[] list;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        list = new int[m];
+
+        dfs(0, 1);
+    }
+
+    public static void dfs(int count, int start) {
+        if (count == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(list[i] + " ");
+            }
+            System.out.println("");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            list[count] = i;
+            dfs(count + 1, i);
+        }
+    }
+}
