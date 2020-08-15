eae3e9306cb164baae85073c9d838c1f9625e7b5
LeeJuneHyun <dlwnsgus777@naver.com>
2020-08-16 08:32:12 +0900

    feat: 백준 알고리즘 문제 풀이 중

--- baekjoon/src/main/java/basic/backTraking/NQueen.java
+++ baekjoon/src/main/java/basic/backTraking/NQueen.java
@@ -1,4 +1,14 @@
 package basic.backTraking;
 
+import java.util.Scanner;
+
 public class NQueen {
+    static int n;
+    static boolean[][] check;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        check = new boolean[n][n];
+    }
 }
