29cb95c11aca8acd86652d1870951ca5d5e08678
LeeJuneHyun <dlwnsgus777@naver.com>
2020-08-23 12:17:32 +0900

    feat: 백준 알고리즘 문제 풀이 중

--- baekjoon/src/main/java/basic/backTraking/Sudoku.java
+++ baekjoon/src/main/java/basic/backTraking/Sudoku.java
@@ -1,7 +1,9 @@
 package basic.backTraking;
 
+import java.util.Scanner;
+
 public class Sudoku {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
     }
 }
