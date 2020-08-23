81a695ec38512c462ac9c26d479f4d09e1fdbea4
Lee, JunHyun <dlwnsgus777@naver.com>
2020-08-24 08:37:02 +0900

    feat: 백준 알고리즘 풀이 중

---
+++ baekjoon/src/main/java/basic/backTraking/Operator.java
@@ -0,0 +1,9 @@
+package basic.backTraking;
+
+import java.util.Scanner;
+
+public class Operator {
+    public static void main(String[] args) {
+        
+    }
+}
--- baekjoon/src/main/java/basic/backTraking/Sudoku.java
+++ baekjoon/src/main/java/basic/backTraking/Sudoku.java
@@ -1,9 +1,29 @@
 package basic.backTraking;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sudoku {
+    static int[][] table;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        table = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+                int num = Integer.parseInt(sc.next());
+
+                table[i][j] = num;
+            }
+        }
+
+        for(int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
