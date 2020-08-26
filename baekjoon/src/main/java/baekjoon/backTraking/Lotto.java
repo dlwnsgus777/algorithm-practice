af2e5bce88f1a81e5f35e137b60b0e2b01d32144
Lee, JunHyun <dlwnsgus777@naver.com>
2020-08-27 08:30:05 +0900

    feat: 백준 알고리즘 문제 풀이 중

--- baekjoon/src/main/java/baekjoon/backTraking/Lotto.java
+++ baekjoon/src/main/java/baekjoon/backTraking/Lotto.java
@@ -1,9 +1,26 @@
 package baekjoon.backTraking;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lotto {
+    static int[] lotto;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            lotto = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                lotto[i] = sc.nextInt();
+            }
+
+            System.out.println(Arrays.toString(lotto));
+        }
     }
 }
