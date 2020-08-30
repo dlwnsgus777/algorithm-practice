4dc550255d54c876ec9f4d243d2150536e6d9561
LeeJuneHyun <dlwnsgus777@naver.com>
2020-08-30 11:25:31 +0900

    feat: 백준 알고리즘 문제 풀이

--- baekjoon/src/main/java/baekjoon/backTraking/SequenceSum.java
+++ baekjoon/src/main/java/baekjoon/backTraking/SequenceSum.java
@@ -1,7 +1,38 @@
 package baekjoon.backTraking;
 
+import java.util.Scanner;
+
 public class SequenceSum {
+    static int n, s, result;
+    static int[] num;
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        s = sc.nextInt();
+
+        num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        result = 0;
+        for (int i = 0; i < n; i++) {
+            dfs(num[i], i);
+        }
+        System.out.println(result);
+    }
+
+    public static void dfs(int sum, int count) {
+        if (sum == s && count == n - 1) {
+            result++;
+            return;
+        }
+        count++;
+
+        if (count < n) {
+            dfs(sum + num[count], count);
+            dfs(sum, count);
+        }
     }
 }
