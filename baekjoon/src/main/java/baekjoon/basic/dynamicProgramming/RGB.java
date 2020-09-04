70231300cb37060e5f3352225653f1b70361b32c
LeeJuneHyun <dlwnsgus777@naver.com>
2020-09-05 07:35:45 +0900

    feat: 백준 알고리즘 문제 풀이 중

---
+++ baekjoon/src/main/java/baekjoon/basic/dynamicProgramming/RGB.java
@@ -0,0 +1,10 @@
+package baekjoon.basic.dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RGB {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
