6f1695949c2feb464561afa28bf1073b42db3d26
LeeJuneHyun <dlwnsgus777@naver.com>
2020-09-07 08:59:54 +0900

    feat: 자바 알고리즘 문제 풀이 중

--- baekjoon/src/main/java/baekjoon/basic/dynamicProgramming/RGB.java
+++ baekjoon/src/main/java/baekjoon/basic/dynamicProgramming/RGB.java
@@ -1,10 +1,27 @@
 package baekjoon.basic.dynamicProgramming;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class RGB {
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] house = new int[n][3];
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                house[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(house[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
