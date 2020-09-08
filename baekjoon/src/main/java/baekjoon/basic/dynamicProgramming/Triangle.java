7ef7e3c544a95a814c7fdec78fe937cf8be8644a
LeeJuneHyun <dlwnsgus777@naver.com>
2020-09-09 08:13:46 +0900

    feat: 백준 알고리즘 문제 풀이 중

--- baekjoon/src/main/java/baekjoon/basic/dynamicProgramming/Triangle.java
+++ baekjoon/src/main/java/baekjoon/basic/dynamicProgramming/Triangle.java
@@ -1,4 +1,13 @@
 package baekjoon.basic.dynamicProgramming;
 
+import java.util.Scanner;
+
 public class Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        
+    }
 }
