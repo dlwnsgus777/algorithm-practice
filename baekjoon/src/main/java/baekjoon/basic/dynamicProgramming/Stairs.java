847adb2e7cdf8ca5c604698a91fee2a4bbacb450
Lee, JunHyun <dlwnsgus777@naver.com>
2020-09-21 08:17:04 +0900

    feat: 백준 알고리즘 문제 풀이 중

---
+++ baekjoon/src/main/java/baekjoon/basic/dynamicProgramming/Stairs.java
@@ -0,0 +1,23 @@
+package baekjoon.basic.dynamicProgramming;
+
+import java.util.Scanner;
+
+public class Stairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] stair = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            stair[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 
+        }
+
+    }
+}
