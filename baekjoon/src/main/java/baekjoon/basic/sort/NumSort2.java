76c0b55a7b3df180a74a09a08fe5bc31fada40b4
Ted <dlwnsgus777@naver.com>
2022-11-22 16:18:41 +0900

    feat: 알고리즘 문제 풀이
    feat: 알고리즘 문제 풀이

--- baekjoon/src/main/java/baekjoon/basic/sort/NumSort2.java
+++ baekjoon/src/main/java/baekjoon/basic/sort/NumSort2.java
@@ -1,23 +1,34 @@
 package baekjoon.basic.sort;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class NumSort2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
         }
 
-        Arrays.sort(arr);
+        Collections.sort(list);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (Integer integer : list) {
+            bw.write(integer + "\n");
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
