60baec18296fde5b551afcf974d73cd725d53f55
Ted <dlwnsgus777@naver.com>
2022-12-27 16:18:19 +0900

    feat: 알고리즘 문제 풀이
    feat: 알고리즘 문제 풀이

--- baekjoon/src/main/java/baekjoon/basic/math2/팩토리얼0의개수.java
+++ baekjoon/src/main/java/baekjoon/basic/math2/팩토리얼0의개수.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.HashSet;
 
 public class 팩토리얼0의개수 {
    public static void main(String[] args) throws IOException {
---
+++ baekjoon/src/main/java/programmers/햄버거만들기.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.Stack;
+
+// 풀이 참고
+public class 햄버거만들기 {
+   public static void main(String[] args) {
+      int[] arr = {1, 3, 2, 1, 2, 1, 3, 1, 2};
+      solution(arr);
+   }
+
+   public static int solution(int[] ingredient) {
+      Stack<Integer> stack = new Stack<>();
+      int answer = 0;
+
+      for (int i = 0; i < ingredient.length; i++) {
+         stack.push(ingredient[i]);
+
+         if (stack.size() >= 4) {
+            if (stack.get(stack.size() - 4) == 1
+            && stack.get(stack.size() - 3) == 2
+            && stack.get(stack.size() - 2) == 3
+            &&stack.get(stack.size() - 1) == 1) {
+               answer++;
+               stack.pop();
+               stack.pop();
+               stack.pop();
+               stack.pop();
+            }
+         }
+      }
+      return answer;
+   }
+}
