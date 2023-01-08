0f511d3519272c8a2eff7d4906acd85721ce5b4a
Ted <dlwnsgus777@naver.com>
2023-01-09 08:43:47 +0900

    feat: 불필요 폴더 삭제
    feat: 불필요 폴더 삭제

--- baekjoon-py/그리디/폴리오미노.py
+++
@@ -1,9 +0,0 @@
-n = str(input())
-
-n = n.replace("XXXX", "AAAA")
-n = n.replace("XX", "BB")
-
-if "X" in n:
-  print(-1)
-else:
-  print(n)
--- 이것이코딩테스트다/그리디/3-3_숫자카드게임.py
+++
@@ -1,9 +0,0 @@
-n, m = map(int,input().split())
-
-result = 0
-for i in range(n):
-  data = list(map(int,input().split()))
-  minNumber = min(data)
-  result = max(result, minNumber)
-
-print(result)
