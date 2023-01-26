287a2839e2dcb41572ba91ff05b8dc85f56ed14c
Ted <dlwnsgus777@naver.com>
2023-01-26 09:46:55 +0900

    feat: 알고리즘 문제 풀이
    feat: 알고리즘 문제 풀이

---
+++ practice/src/main/java/programmers/JadenCase문자열.java
@@ -0,0 +1,74 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+// 풀이 참고함
+public class JadenCase문자열 {
+   public static void main(String[] args) {
+      System.out.println(solution(" 3peoPle aaa"));
+   }
+   public static String solution(String s) {
+      String[] str = s.split(" ");
+
+      for (int i = 0; i < str.length; i++) {
+         String trimStr = str[i].trim();
+
+         if (trimStr.isEmpty()) {
+            continue;
+         }
+
+         String string = trimStr.toLowerCase();
+
+         if (!Character.isDigit(string.charAt(0))) {
+
+            char first = string.charAt(0);
+
+            String result = string.replaceFirst(String.valueOf(first), String.valueOf(Character.toUpperCase(first)));
+
+            str[i] = result;
+         } else {
+            str[i] = string;
+         }
+      }
+
+      return Arrays.stream(str).filter(st -> !st.isEmpty()).collect(Collectors.joining(" "));
+   }
+}
+
+
+/*
+*         String answer = "";
+
+    	// " " 기준으로 문자열 잘라서 배열에 넣음
+    	String[] arr = s.split(" ");
+    	// 잘린 문자열을 순서대로 처리
+    	for(int i=0; i<arr.length; i++) {
+    		String now = arr[i];
+
+    		// 문자열의 길이가 0이라면(공백 이라면)
+    		// answer에 " "만 추가
+    		if(arr[i].length() == 0) {
+    			answer += " ";
+    		}
+    		// 문자가 있다면
+    		else {
+    			// 0번째 문자는 대문자로
+    			answer += now.substring(0, 1).toUpperCase();
+    			// 1번째 문자부터 마지막까지는 소문자로
+    			answer += now.substring(1, now.length()).toLowerCase();
+    			// 마지막에 " " 추가
+    			answer += " ";
+    		}
+
+    	}
+
+    	// 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
+    	if(s.substring(s.length()-1, s.length()).equals(" ")){
+    		return answer;
+    	}
+
+    	// 맨 마지막 " " 제거하고 answer 반환
+        return answer.substring(0, answer.length()-1);
+*
+* */
